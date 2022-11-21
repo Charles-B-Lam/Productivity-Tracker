804a156ca9f7f5ac67f58648630aadf4a0780f3b
Tommy <tjfricano@gmail.com>
2022-11-21 18:23:03 -0500

    working todo backend- dummy tests

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,50 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+
+// app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//     const error = new HttpError('Could not find this route.', 404);
+//     throw error;
+//   });
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
+const todosRoutes = require('./routes/todos-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use('/api/todos', todosRoutes); // => /api/places...
 
-  next();
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
 });
 
-app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
-  });
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+app.listen(5050);
