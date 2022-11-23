6167af8fa55518c1ec58959676f327bc24b31a92
Tommy <tjfricano@gmail.com>
2022-11-22 20:28:16 -0500

    working todo calls

--- backend/controllers/todos-controllers.js
+++ backend/controllers/todos-controllers.js
@@ -3,31 +3,9 @@ const HttpError = require('../models/http-error');
 const { validationResult } = require('express-validator');
 const Todo = require('../models/task');
 
-let DUMMY_TODOS = [
-    {
-      id: 'p1',
-      text: 'Homework',
-      status: 'In progress',
-      start: '11/20/2022',
-      end: '11/28/2022',
-      priority: 'Low'
-    }
-  ];
-
-// const getTodoById = (req, res, next) => {
-//     const todoId = req.params.pid; // { pid: 'p1' }
-  
-//     const todo = DUMMY_TODOS.find(p => {
-//       return p.id === todoId;
-//     });
-  
-//     if (!todo) {
-//       throw new HttpError('Could not find a todo for the provided id.', 404);
-//     }
-  
-//     res.json({ todo }); 
-//   };
-
+  /*
+    get todo api call
+  */
   const getTodoById = async (req, res, next) => {
     const todoId = req.params.pid; 
     let todo;
@@ -52,22 +30,9 @@ let DUMMY_TODOS = [
     res.json({ todo: todo.toObject({ getters: true }) }); // => { place } => { place: place }
   };
 
-  // const createTodo = (req, res, next) => {
-  //   const { text, status, start, end, priority } = req.body;
-  //   const createdTodo = {
-  //     id: uuid(),
-  //     text,
-  //     status,
-  //     start,
-  //     end,
-  //     priority
-  //   };
-  
-  //   DUMMY_TODOS.push( createdTodo ); 
-  
-  //   res.status(201).json({ todo: createdTodo });
-  // };
-
+  /*
+    update todo api call
+  */
   const createTodo = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -99,30 +64,79 @@ let DUMMY_TODOS = [
     res.status(201).json({ todo: createdTodo });
   };
   
-
-  const updateTodo = (req, res, next) => {
-    const { text, status, start, end, priority } = req.body;
+  /*
+    update todo api call
+  */
+  const updateTodo = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    }
+  
+    const { status, text, start, end, priority } = req.body;
     const todoId = req.params.pid;
   
-    const updatedTodo = { ...DUMMY_TODOS.find(p => p.id === todoId) };
-    const todoIndex = DUMMY_TODOS.findIndex(p => p.id === todoId);
-    updatedTodo.text = text;
-    updatedTodo.status = status;
-    updatedTodo.start = start;
-    updatedTodo.end = end;
-    updatedTodo.priority = priority;
+    let todo;
+    try {
+      todo = await Todo.findById(todoId);
+    } catch (err) {
+      const error = new HttpError(
+        'Something went wrong, could not update place.',
+        500
+      );
+      return next(error);
+    }
   
-    DUMMY_TODOS[todoIndex] = updatedTodo;
+    todo.text = text;
+    todo.status = status;
+    todo.start = start;
+    todo.end = end;
+    todo.priority = priority;
   
-    res.status(200).json({ todo: updatedTodo });
+    try {
+      await todo.save();
+    } catch (err) {
+      const error = new HttpError(
+        'Something went wrong, could not update place.',
+        500
+      );
+      return next(error);
+    }
+  
+    res.status(200).json({ todo: todo.toObject({ getters: true }) });
   };
-  
-  const deleteTodo = (req, res, next) => {
+
+  /*
+    Delete todo api call
+  */
+  const deleteTodo = async (req, res, next) => {
     const todoId = req.params.pid;
-    DUMMY_TODOS = DUMMY_TODOS.filter(p => p.id !== todoId);
+  
+    let place;
+    try {
+      todo = await Todo.findById(todoId);
+    } catch (err) {
+      const error = new HttpError(
+        'Something went wrong, could not delete place.',
+        500
+      );
+      return next(error);
+    }
+  
+    try {
+      await todo.remove();
+    } catch (err) {
+      const error = new HttpError(
+        'Something went wrong, could not delete place.',
+        500
+      );
+      return next(error);
+    }
+  
     res.status(200).json({ message: 'Deleted todo.' });
   };
 
+  // export all api calls
   exports.getTodoById = getTodoById;
   exports.createTodo = createTodo;
   exports.updateTodo = updateTodo;
